778b9d14486775d1b8c0c7b7c37fddb79ac461f6
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-06 06:23:30 +0000

    ADD player name inputs start game msg

--- main.rb
+++ main.rb
@@ -7,3 +7,12 @@ require './turns.rb'
 
 
 puts "Welcome to the Math Game!"
+puts "Player 1, please enter your name: "
+player1 = gets.chomp
+puts "Player 2, please enter your name: "
+player2 = gets.chomp
+
+puts "🎲 Let's get ready to rumble! 🎲"
+game = Game.new(player1, player2)
+game.start_game
+
